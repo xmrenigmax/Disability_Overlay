1331dd2172ec3a7b72ff1777eb8ede54a9a63a0d
xmrenigmax <Rileyjordan@hotmail.com>
2024-11-20 16:35:56 +0000

    Remove obsolete files and clean up the project structure

--- Disabilitiy_Phone_Overlay/camera_overlay.py
+++
@@ -1,8 +0,0 @@
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.camera import Camera
-
-class CameraOverlay(FloatLayout):
-    def __init__(self, **kwargs):
-        super(CameraOverlay, self).__init__(**kwargs)
-        self.camera = Camera(play=True)
-        self.add_widget(self.camera)
